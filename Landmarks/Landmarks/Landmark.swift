28a1f46071563092a0015f4e7ec5122cf0b93ce7
Noah Bardwell <noah.bardwell123@gmail.com>
2024-03-13 19:15:07 -0500

    starting building lists and navigations

---
+++ Landmarks/Landmarks/Landmark.swift
@@ -0,0 +1,30 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Noah Bardwell on 3/13/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+    
+}
