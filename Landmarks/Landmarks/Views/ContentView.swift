cc2b2a421e52b66ba8233739f026d27f1f6effa8
Noah Bardwell <noah.bardwell123@gmail.com>
2024-03-15 17:02:27 -0500

    Ending at section 8

--- Landmarks/Landmarks/Views/ContentView.swift
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -9,18 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationSplitView {
-            List(landmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail()
-                } label: {
-                    LandmarkRow(landmark: landmark)
-                }
-            }
-            .navigationTitle("Landmarks")
-        } detail: {
-            Text("Select a Landmark")
-        }
+        LandmarkList()
     }
 }
 
